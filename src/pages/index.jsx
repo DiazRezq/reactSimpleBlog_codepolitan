63127ff5a45a86c95311864e2e57bb586794a5f6
Diaz Rezqi Firmansyah <diaz.rezqi@gmail.com>
2024-01-31 19:22:13 +0700

    search feature with useState

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import posts from "../posts.json";
 import Articles from "../components/Articles";
 function Homepage() {
+  const [search, setSearch] = useState("");
+
+  const changeSearch = (event) => {
+    setSearch(event.target.value);
+  };
   return (
     <>
       <h1>Simple Blog</h1>
-      {posts.map((blog) => (
-        <Articles title={blog.title} tags={blog.tags} date={blog.date} />
+      <div>
+        cari artikel : <input type="text" onChange={changeSearch} />
+      </div>
+      <small>Ditemukan 0 data dari kata {search}</small>
+      {posts.map(({ title, tags, date }, index) => (
+        // <Articles title={blog.title} tags={blog.tags} date={blog.date} />
+        <Articles {...{ title, tags, date }} key={index} />
       ))}
     </>
   );

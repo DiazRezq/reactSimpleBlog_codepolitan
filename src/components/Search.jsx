6c5f63be23be384d4818ccfa4347995812f5d3ff
Diaz Rezqi Firmansyah <diaz.rezqi@gmail.com>
2024-02-06 20:18:56 +0700

    Fetch API with useEffect

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,15 +2,26 @@ import { useState } from "react";
 function Search(props) {
   const [search, setSearch] = useState("");
 
-  const onSearchChange = (event) => {
-    setSearch(event.target.value);
-    props.onSearchChange(event.target.value);
+  const onSearchChange = () => {
+    props.onSearchChange(search);
+  };
+
+  const searchKeydown = (e) => {
+    if (e.key === "Enter") {
+      onSearchChange();
+    }
   };
 
   return (
     <>
       <div>
-        cari artikel : <input type="text" onChange={onSearchChange} />
+        cari artikel :{" "}
+        <input
+          type="text"
+          onChange={(e) => setSearch(e.target.value)}
+          onKeyDown={searchKeydown}
+        />{" "}
+        <button onClick={onSearchChange}>Cari</button>
       </div>
       <small>
         Ditemukan {props.totalPosts} data dari kata {search}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import postsData from "../posts.json";
 import Articles from "../components/Articles";
 import Search from "../components/Search";
@@ -6,6 +6,7 @@ import Search from "../components/Search";
 function Homepage() {
   const [posts, setPost] = useState(postsData);
   const [totalPosts, setTotalPosts] = useState(0);
+  const [extenalPosts, setExternalPosts] = useState([]);
 
   const onSearchChange = (value) => {
     const filteredPosts = postsData.filter((item) =>
@@ -14,6 +15,11 @@ function Homepage() {
     setPost(filteredPosts);
     setTotalPosts(filteredPosts.length);
   };
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setExternalPosts(json));
+  }, []);
   return (
     <>
       <h1>Simple Blog</h1>
@@ -22,6 +28,11 @@ function Homepage() {
         // <Articles title={blog.title} tags={blog.tags} date={blog.date} />
         // <Articles {...{ title, tags, date }} key={index} />
         <Articles {...props} key={index} />
+      ))}
+      <hr />
+      <h2>External Posts</h2>
+      {extenalPosts.map((item, index) => (
+        <div key={index}> - {item.title}</div>
       ))}
     </>
   );
